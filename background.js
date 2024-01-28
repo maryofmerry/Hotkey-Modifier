ac546a526dcc908ba097b1c87c07c675ee904534
maryofmerry <157926878+maryofmerry@users.noreply.github.com>
2024-01-28 13:00:16 -0800

    Added Manifest json file and background js file
    Manifest describes everything, background fetches hotkey and (just for now) responds it back

---
+++ background.js
@@ -0,0 +1,14 @@
+let hotkeyEnabled = false;
+
+function toggleHotkey() {
+  hotkeyEnabled = !hotkeyEnabled;
+  alert(`Hotkey is ${hotkeyEnabled ? 'enabled' : 'disabled'}.`);
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.commands.onCommand.addListener((command) => {
+    if (command === 'toggle-hotkey') {
+      toggleHotkey();
+    }
+  });
+});
